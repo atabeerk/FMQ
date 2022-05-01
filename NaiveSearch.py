1d48a4d477643f57c569bc554a7c14d3b38b36dc
Ataberk Donmez <ataberdonmez@gmail.com>
2022-05-01 19:31:42 -0400

    implement NaiveSearch, sanity check with comparing PatternTable

--- NaiveSearch.py
+++ NaiveSearch.py
@@ -1,10 +1,21 @@
 
 
 class NaiveSearch:
-    def __init__(self):
-        pass
+    def __init__(self, raw_matrix):
+        self.raw_matrix = raw_matrix
 
-    def search(self, queries):
-        # todo@team
-        pass
+    def search(self, query):
+        result = set()
+        symbol_values = {}
+        # substitute each binary symbol with its corresponding value for each row
+        for i, row in enumerate(self.raw_matrix):
+            for symbol in query.binary_symbols:
+                symbol_values[symbol] = row[self.__get_column(symbol)]
+            if query.subs(symbol_values):
+                result.add(i)
 
+        return result
+
+    @staticmethod
+    def __get_column(symbol):
+        return int(str(symbol)[1:])
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from QueryExpander import QueryExpander
 from PatternTable import PatternTable
 from QueryParser import QueryParser
 from QueryGenerator import QueryGenerator
+from NaiveSearch import NaiveSearch
 
 
 
@@ -12,16 +13,25 @@ from QueryGenerator import QueryGenerator
 qe = QueryExpander()
 qg = QueryGenerator()
 
-# query = qg.naive_generator(5)
-query = qg.expr_generator(5, 3)
-# query = qg.or_generator(5)
-print(query)
-queries = qe.expand(query, 5)
+for i in range(500):
+    # query = qg.naive_generator(5)
+    query = qg.expr_generator(5, 3)
+    # query = qg.or_generator(5)
+    # print(query)
+    queries = qe.expand(query, 5)
 
-data = list(np.random.randint(2, size=(10, 5)))
-pt = PatternTable(data)
-result = pt.search(queries)
+    data = list(np.random.randint(2, size=(10, 5)))
+    pt = PatternTable(data)
+    ns = NaiveSearch(data)
 
-print(result)
+    pt_result = pt.search(queries)
+    ns_result = ns.search(query)
+    if not(pt_result == ns_result):
+        print(data)
+        print(pt_result)
+        print(ns_result)
+
+
+
 
 
